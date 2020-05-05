25e926117f0485802ffc62faf7dc2dfe40c37a41
unknown <aminmirzoev123@gmail.com>
2020-05-05 05:35:12 +0500

    create class and librarys

--- Program.cs
+++ Program.cs
@@ -1,5 +1,9 @@
 ﻿using System;
-
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
 
 namespace HW16_03_05
 {
@@ -7,13 +11,19 @@ namespace HW16_03_05
     {
         static void Main(string[] args)
         {
-            
-            
+
+         
+
+
         }
-
-    }
-
-}
        
         
+   }
 
+     class shifts
+     {
+         public int left{get;set;}
+         public int Top{get;set;} 
+         public char[] simvol { get; set; }
+     }
+}
