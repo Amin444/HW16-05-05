9adf4f8cac43cf8374512c57a17c40f23385b33e
unknown <aminmirzoev123@gmail.com>
2020-05-05 05:28:38 +0500

    starter

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+
+namespace HW16_03_05
+{
+ class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            
+        }
+
+    }
+
+}
+       
+        
+
